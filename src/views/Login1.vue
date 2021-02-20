21bc056865284521e419abc5eae586e51ab14fb1
Aleksandr Efremov <sasha.fima@gmail.com>
2021-02-20 19:06:19 +0300

    Сдела старницу логина

--- src/views/Login1.vue
+++ src/views/Login1.vue
@@ -1,25 +1,174 @@
 <template>
-  <div>
-    привет.
-    <b-card>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel eum tenetur
-      ipsam inventore officiis! Dolore ab expedita nam, nulla quas, et eos
-      quibusdam perferendis nisi quod beatae, nesciunt odit tempora.</b-card>
-    <b-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse ipsa
-      officiis fugit illo saepe enim voluptatem dolores suscipit, doloribus
-      facilis a.</b-card-text>
+  <div class="auth-wrapper auth-v1 px-2">
+    <div class="auth-inner py-2">
+
+      <!-- Login v1 -->
+      <b-card class="mb-0">
+        <b-link class="brand-logo">
+          <vuexy-logo />
+
+        </b-link>
+
+        <b-card-title class="mb-1">
+          Добро пожаловать!
+        </b-card-title>
+
+        <b-card-text class="mb-2">
+          Введите номер вашей топливной карты и пароль.
+        </b-card-text>
+
+        <!-- form -->
+        <validation-observer
+          ref="loginForm"
+          #default="{invalid}"
+        >
+          <b-form
+            class="auth-login-form mt-2"
+            @submit.prevent
+          >
+
+            <!-- email -->
+            <b-form-group
+              label-for="email"
+              label="Номер карты"
+            >
+              <validation-provider
+                #default="{ errors }"
+                name="Email"
+                rules="required|email"
+              >
+                <b-form-input
+                  id="email"
+                  v-model="userEmail"
+                  name="login-email"
+                  :state="errors.length > 0 ? false:null"
+                  placeholder="7824000011112223333"
+                  autofocus
+                />
+                <small class="text-danger">{{ errors[0] }}</small>
+              </validation-provider>
+            </b-form-group>
+
+            <!-- password -->
+            <b-form-group>
+              <div class="d-flex justify-content-between">
+                <label for="password">Пароль</label>
+              </div>
+              <validation-provider
+                #default="{ errors }"
+                name="Пароль"
+                rules="required"
+              >
+                <b-input-group
+                  class="input-group-merge"
+                  :class="errors.length > 0 ? 'is-invalid':null"
+                >
+                  <b-form-input
+                    id="password"
+                    v-model="password"
+                    :type="passwordFieldType"
+                    class="form-control-merge"
+                    :state="errors.length > 0 ? false:null"
+                    name="login-password"
+                    placeholder="Пароль"
+                  />
+
+                  <b-input-group-append is-text>
+                    <feather-icon
+                      class="cursor-pointer"
+                      :icon="passwordToggleIcon"
+                      @click="togglePasswordVisibility"
+                    />
+                  </b-input-group-append>
+                </b-input-group>
+                <small class="text-danger">{{ errors[0] }}</small>
+              </validation-provider>
+            </b-form-group>
+
+            <!-- checkbox -->
+            <b-form-group>
+              <b-form-checkbox
+                id="remember-me"
+                v-model="status"
+                name="checkbox-1"
+              >
+                Запомнить пароль
+              </b-form-checkbox>
+            </b-form-group>
+
+            <!-- submit button -->
+            <b-button
+              variant="primary"
+              type="submit"
+              block
+              :disabled="invalid"
+            >
+              Войти
+            </b-button>
+          </b-form>
+        </validation-observer>
+      </b-card>
+      <!-- /Login v1 -->
+    </div>
   </div>
 </template>
 
 <script>
-import { BCard, BCardText } from 'bootstrap-vue'
+import { ValidationProvider, ValidationObserver } from 'vee-validate'
+import {
+  BButton,
+  BForm,
+  BFormInput,
+  BFormGroup,
+  BCard,
+  BLink,
+  BCardTitle,
+  BCardText,
+  BInputGroup,
+  BInputGroupAppend,
+  BFormCheckbox,
+} from 'bootstrap-vue'
+import VuexyLogo from '@core/layouts/components/Logo.vue'
+import { required, email } from '@validations'
+import { togglePasswordVisibility } from '@core/mixins/ui/forms'
 
 export default {
   components: {
+    // BSV
+    BButton,
+    BForm,
+    BFormInput,
+    BFormGroup,
     BCard,
+    BCardTitle,
+    BLink,
+    VuexyLogo,
     BCardText,
+    BInputGroup,
+    BInputGroupAppend,
+    BFormCheckbox,
+    ValidationProvider,
+    ValidationObserver,
+  },
+  mixins: [togglePasswordVisibility],
+  data() {
+    return {
+      userEmail: '',
+      password: '',
+      status: '',
+      // validation rules
+      required,
+      email,
+    }
+  },
+  computed: {
+    passwordToggleIcon() {
+      return this.passwordFieldType === 'password' ? 'EyeIcon' : 'EyeOffIcon'
+    },
   },
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "@core/scss/vue/pages/page-auth.scss";
 </style>
