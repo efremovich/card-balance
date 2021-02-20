4c924099497d0f9773f3bd11f5e201069d7d5a42
Aleksandr Efremov <sasha.fima@gmail.com>
2021-02-20 16:50:17 +0300

    Старт проекта Мегаполись Краснодар

---
+++ src/views/Login1.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    привет.
+    <b-card>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel eum tenetur
+      ipsam inventore officiis! Dolore ab expedita nam, nulla quas, et eos
+      quibusdam perferendis nisi quod beatae, nesciunt odit tempora.</b-card>
+    <b-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse ipsa
+      officiis fugit illo saepe enim voluptatem dolores suscipit, doloribus
+      facilis a.</b-card-text>
+  </div>
+</template>
+
+<script>
+import { BCard, BCardText } from 'bootstrap-vue'
+
+export default {
+  components: {
+    BCard,
+    BCardText,
+  },
+}
+</script>
+
+<style>
+</style>
