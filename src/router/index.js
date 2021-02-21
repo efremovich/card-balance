36107eb0b186fddb1c6c81643b50064d75dd4a2a
Aleksandr Efremov <sasha.fima@gmail.com>
2021-02-21 18:41:39 +0300

    Авторизация и регистрация пользователей и роутинг

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+// Routes
+import { canNavigate } from '@/libs/acl/routeProtection'
+import { isUserLoggedIn, getUserData, getHomeRouteForLoggedInUser } from '@/auth/utils'
+import pages from './pages'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
@@ -10,55 +15,33 @@ const router = new VueRouter({
     return { x: 0, y: 0 }
   },
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('@/views/Home.vue'),
-      meta: {
-        pageTitle: 'Home',
-        breadcrumb: [
-          {
-            text: 'Home',
-            active: true,
-          },
-        ],
-      },
-    },
-    {
-      path: '/second-page',
-      name: 'second-page',
-      component: () => import('@/views/SecondPage.vue'),
-      meta: {
-        pageTitle: 'Second Page',
-        breadcrumb: [
-          {
-            text: 'Second Page',
-            active: true,
-          },
-        ],
-      },
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('@/views/Login1.vue'),
-      meta: {
-        layout: 'full',
-      },
-    },
-    {
-      path: '/error-404',
-      name: 'error-404',
-      component: () => import('@/views/error/Error404.vue'),
-      meta: {
-        layout: 'full',
-      },
-    },
+    { path: '/', redirect: { name: 'dashboard' } },
+    ...pages,
     {
       path: '*',
       redirect: 'error-404',
     },
   ],
+})
+
+router.beforeEach((to, _, next) => {
+  const isLoggedIn = isUserLoggedIn()
+
+  if (!canNavigate(to)) {
+    // Redirect to login if not logged in
+    if (!isLoggedIn) return next({ name: 'auth-login' })
+
+    // If logged in => not authorized
+    return next({ name: 'misc-not-authorized' })
+  }
+
+  // Redirect if logged in
+  if (to.meta.redirectIfLoggedIn && isLoggedIn) {
+    const userData = getUserData()
+    next(getHomeRouteForLoggedInUser(userData ? userData.role : null))
+  }
+
+  return next()
 })
 
 export default router
---
+++ src/router/pages.js
@@ -0,0 +1,57 @@
+export default [
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: () => import('@/views/Home.vue'),
+  },
+  {
+    path: '/error-404',
+    name: 'error-404',
+    component: () => import('@/views/error/Error404.vue'),
+    meta: {
+      layout: 'full',
+      resource: 'Auth',
+      action: 'read',
+    },
+  },
+  {
+    path: '/login',
+    name: 'auth-login',
+    component: () => import('@/views/auth/Login.vue'),
+    meta: {
+      layout: 'full',
+      resource: 'Auth',
+      redirectIfLoggedIn: true,
+    },
+  },
+  {
+    path: '/register',
+    name: 'auth-register',
+    component: () => import('@/views/auth/Register.vue'),
+    meta: {
+      layout: 'full',
+      resource: 'Auth',
+      redirectIfLoggedIn: true,
+    },
+  },
+  {
+    path: '/forgot-password',
+    name: 'auth-forgot-password',
+    component: () => import('@/views/auth/ForgotPassword.vue'),
+    meta: {
+      layout: 'full',
+      resource: 'Auth',
+      redirectIfLoggedIn: true,
+    },
+  },
+  {
+    path: '/reset-password',
+    name: 'auth-reset-password',
+    component: () => import('@/views/auth/ResetPassword.vue'),
+    meta: {
+      layout: 'full',
+      resource: 'Auth',
+      redirectIfLoggedIn: true,
+    },
+  },
+]
