5bada925fe1765ac992a24d53ce7ec34560e28c4
Efremov Aleksand <sasha.fima@gmail.com>
2022-06-07 14:38:09 +0300

    fix

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 const axiosIns = axios.create({
   // You can add your headers here
   // ================================
-  baseURL: 'http://server.re-base.ru:8000',
+  baseURL: 'https://api.cargo.re-base.ru:8000',
   timeout: 10000,
   // headers: {'X-Custom-Header': 'foobar'}
 });
