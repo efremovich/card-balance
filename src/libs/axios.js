678663f93681a88153eb3e7668d8d0784b44b99b
Efremov Aleksand <sasha.fima@gmail.com>
2022-06-10 10:05:59 +0300

    Изменение настроек сервера в axios

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -6,11 +6,11 @@ import axios from 'axios';
 const axiosIns = axios.create({
   // You can add your headers here
   // ================================
-  baseURL: 'https://api.cargo.re-base.ru:8000',
+  baseURL: 'https://re-base.ru/',
   timeout: 10000,
+  // headers: { 'Access-Control-Allow-Origin': '*' },
   // headers: {'X-Custom-Header': 'foobar'}
 });
-
 Vue.prototype.$http = axiosIns;
 
 export default axiosIns;
