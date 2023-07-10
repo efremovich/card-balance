078be5c57bc86e27427af56a55aba650d846a05d
Igo87 <bajrushev@gmail.com>
2023-07-10 15:47:39 +0300

    fix holder without quotes and spaces

--- src/views/CardDetails.vue
+++ src/views/CardDetails.vue
@@ -1498,14 +1498,14 @@ export default {
         this.comparison = false;
         this.changeValueHolder = true;
         if (this.saveChange) {
-          const holder = this.cardHolder.replace(/["']/g, '').trim(); // убираю кавычки и лишние пробелы у держателя
+          const holder = String(this.cardHolder.replace(/["']/g, '').trim()); // убираю кавычки и лишние пробелы у держателя
           const request = [{
             card_number: this.cardData.data.number,
             request_type_code: 'RENAME',
             request_status_code: 'CREATED',
             contract_id: this.cardData.data.contract_id,
             // holder: JSON.stringify(`${this.cardHolder}`),
-            holder: JSON.stringify(holder),
+            holder,
           }];
           useJwt.refreshDataUserLimits(request);
         }
--- themeConfig.js
+++ themeConfig.js
@@ -9,7 +9,7 @@ export const $themeBreakpoints = {};
 // APP CONFIG
 export const $themeConfig = {
   app: {
-    appName: 'КарсДрайв', // Will update name in navigation menu (Branding)
+    // appName: 'КарсДрайв', // Will update name in navigation menu (Branding)
     // eslint-disable-next-line global-require
     appLogoImage: require('@/assets/images/logo/logosmall.svg'), // Will update logo in navigation menu (Branding)
   },
