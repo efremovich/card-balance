e7a20542e478fef21b9b01b27e20fb7842b86de2
Igo87 <bajrushev@gmail.com>
2023-07-10 16:46:42 +0300

    some fix holder - trim

--- src/views/CardDetails.vue
+++ src/views/CardDetails.vue
@@ -1498,13 +1498,13 @@ export default {
         this.comparison = false;
         this.changeValueHolder = true;
         if (this.saveChange) {
-          const holder = this.cardHolder.trim(); // убираю кавычки и лишние пробелы у держателя
+          // const holder = this.cardHolder.trim(); // убираю кавычки и лишние пробелы у держателя
           const request = [{
             card_number: this.cardData.data.number,
             request_type_code: 'RENAME',
             request_status_code: 'CREATED',
             contract_id: this.cardData.data.contract_id,
-            holder: JSON.stringify(`${holder}`),
+            holder: JSON.stringify(`${this.cardHolder.trim()}`),
           }];
           useJwt.refreshDataUserLimits(request);
         }
