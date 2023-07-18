b1a29af72b5c8c2bd53a4b68a17502e904f5ae28
Igo87 <bajrushev@gmail.com>
2023-07-18 14:52:52 +0300

    Fix

--- src/views/CardDetails.vue
+++ src/views/CardDetails.vue
@@ -1498,7 +1498,8 @@ export default {
         this.comparison = false;
         this.changeValueHolder = true;
         if (this.saveChange) {
-          const holder = (this.cardHolder.trim()); // убираю кавычки и лишние пробелы у держателя
+          const holder = (this.cardHolder.replace(/[ '"]+/g, ' ').trim()); // убираю кавычки и лишние пробелы у держателя
+          console.log('holder', holder, holder.length);
           const request = [{
             card_number: this.cardData.data.number,
             request_type_code: 'RENAME',
