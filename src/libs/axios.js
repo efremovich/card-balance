70e4d556e9364ec9aaa18e2abe5fe0da2e7f4b2d
Efremov Aleksand <sasha.fima@gmail.com>
2023-10-18 16:54:15 +0300

    fix(axios):Исправление NS_ERROR_DOM_BAD_URI

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -1,17 +1,15 @@
-import Vue from 'vue';
+import Vue from "vue";
 
 // axios
-import axios from 'axios';
+import axios from "axios";
 
 const axiosIns = axios.create({
-  // You can add your headers here
-  // ================================
 
-  baseURL: 'https://xn--e1aybc.xn--80aagg0ac7bbh.xn--p1ai:8888',
+  baseURL: "https://xn--e1aybc.xn--80aagg0ac7bbh.xn--p1ai:8888",
 
   timeout: 10000,
-  // headers: { 'Access-Control-Allow-Origin': '*' },
-  // headers: {'X-Custom-Header': 'foobar'}
+  headers: { "Access-Control-Allow-Origin": "*", "X-Custom-Header": "foobar" }
+
 });
 Vue.prototype.$http = axiosIns;
 
